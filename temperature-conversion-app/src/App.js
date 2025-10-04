edf5f131a3ab504116ce326cf14a8aa2a3c485df
kyawlinthu123 <kyawlinthu655321@gmail.com>
2025-10-04 11:54:55 +0700

    app initialization

---
+++ temperature-conversion-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <form action="">
+        <h1>Temperature Converter </h1>
+      </form>
+    </div>
+  );
+}
+
+export default App;
